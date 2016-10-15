808149e3ef53908f610008a6397f2aa33b413002
Morten Kals <m.kals@me.com>
2016-10-15 15:30:07 -0700

    added calculations for flat belt drives

--- belt_test.m
+++ belt_test.m
@@ -1,67 +1,83 @@
-function [ output ] = belt_test( width, dN_1, dN_2  )
+function [ output ] = belt_test( input  )
+%% For flat belts.
 %% Independent Variables passed to function
-w = imput_args(1); % [inches] width of belt
-n_1 = input_args(4); % [rpm] angular velocity of driving sprocket
-C = input_args(5); % [inches] center distance of sprockets
-h = input_args(6); % [hours] expected lifetime
-S = input_args(7); % Number of strands
-dN_1 = input_args(8); % [in] diameter of driving sprocket
-dN_2 = input_args(9); % [in] diameter of driven sprocket
+% 1 -> small pulley
+% 2 -> large pulley
 
+DN1 =   input(1); % [in] diameter of small pulley
+DN2 =   input(2); % [in] diameter of large pulley
+C =     input(3); % [in] center distance of pulleys
+
+n1 =    input(4); % [rpm] angular velocity of small pulley
+
+b =     input(5); % [in] width of belt
+t =     input(6); % [in] thickness of belt
+
+Hnom =  input(7); % [hp] transmitted power
+Ks =    input(8); % service factor
+nd =    input(9); % design factor, for exigencies
+
+Cp =    input(10); % pulley correction factor (table 17-4, flat belts)
+Cv =    input(11); % velocity correction factor (=1 for polyamide belts)
+
+Fa =    input(12); % [lbf/in] manufacturers' allowed tension, allowable tension per unit width at 600 ft/min (table 17-2)
+gamma = input(13); % [lbf/in^3] spesific weight (table 17-2)
+f =     input(14); % coefficint of friction (table 17-2)
 
 %% Constants
-g = 
+g = 32.17; % [ft/s^2] gravitational acceleration
+
 
 
 %% Belt length calculation
-ThetaN_1 = pi-2*asin((dN_2-dN_1)/(2*C));
-ThetaN_2 = 2*pi-ThetaN_1;
-belt_length = sqrt(4*C^2-(dN_2-dN_1)^2)+1/2*(dN_2*ThetaN_2+dN_1*ThetaN_1);
-
-%% Force calculation
-Fi % initial tension
-Fc % hop tnsion due to centrifugal force
-
-H_transmitted = (F_1 - F_2)V/33000;
+ThetaN_1 = pi-2*asin((DN2-dN1)/(2*C));                                  % [rad] angle of contact for small pulley
+ThetaN_2 = 2*pi-ThetaN_1;                                               % [rad] angle of contact for large pulley
+belt_length = sqrt(4*C^2-(DN2-dN1)^2)+1/2*(DN2*ThetaN_2+dN1*ThetaN_1);  % [in] total belt length 
 
 
 %% Belt drive analisys steps
-% 1 Find exp(f*phi)from belt-drive geometry and friction
+% 1. Find exp(f*phi)from belt-drive geometry and friction
 phi = ThetaN_1;
+exp_f_phi=exp(f,phi);               % useful quantaty for later calculations.
 
-% SUBSTITUTE VALUES FROM table 17-2
-gamma = ;
-f = ; 
-Fa = ;
+%%
+% 2. From belt geometry and speed find Fc
+V = pi*DN1*n1/12;                   % [ft/min] belt speed
+weight_per_length = 12*gamma*b*t;   % [lbf/ft] weight per foot of belt
 
-exp_f_rho=exp(f,rho); % useful quantaty for later calculations.
+Fc = weight_per_length/g*(V/60)^2;  % [lbf] hoop tension due to centrifugal force
+
+% 3. Find necessary torque
+T = 63025*Hnom*Ks*nd/n1;            % [lbf in] transmitted torque 
+
+% 4. From torque T find the necessary (F_1)a - F_2 = 2T/d
+force_difference = 2*T/d;           % [lbf] (F_1)a - F_2
+
+% 5. From Tables 17-2 and 17-4 and eq. 17-12, determine F1a.
+F1a = b*Fa*Cp*Cv;                   % [lbf] allowable largest belt tension
+
+% 6. Find F2 from (F1)a - [(F1)a - F2]
+F2 = F1a-force_difference;          % [lbf] slack side tension in belt
+
+% 7. From Eq. (i) find the necessary initial tension Fi
+Fi = (F1a+F2)/2-Fc;                 % [lbf] initial tension
+
+% 8. Check the friction development, fprime < f. Use eq. 17-7 solved for fprime:
+fprime = 1/phi * log((F1a-Fc)/(F2-Fc)); % developed friction
+
+if (f<fprime)
+    disp('DANGER: developed friction ', fprime, ' exceeds allowed friction ', f);
+else
+    disp('friction', f);
+    disp('developed friction', fprime);
+end
+
+% 9 Find the factor of safety
+Ha = Hnom*Ks*nd;    % [hp] transmitted design power
+nfs = Ha/(Hnom*Ks); % safety factor
 
 
-% 2 From belt geometry and speed find Fc
-Fc = w/g*(V/60)^2; 
+H_transmitted = (F_1 - F_2)V/33000;
 
-% 3 Find necessary torque
-T = 64025*H_nom*K_s*n_d/n;
-
-% 4 From torque T find the necessary (F_1)a - F_2 = 2T/d
-force_difference = 2T/d;
-
-% 5 From Tables 17?2 and 17?4, and Eq. (17?12) determine (F_1)a.
-b = ;
-F_A = ;
-C_P = ;
-C_V = ;
-F_1a = b*F_A*C_p*C_v;
-
-% 6 Find F2 from (F1)a - [(F1)a - F2]
-F2 = F1a-force_difference;
-
-% 7 From Eq. (i) find the necessary initial tension Fi
-Fi = (F1a+F2)/2-Fc;
-
-% 8 Check the friction development, f9 , f. Use Eq. (17?7) solved for f9:
-fprime = 1/rho * log((F1a-Fc)/(F2-Fc));
-
-% 9 Find the factor of safety from nfs 5 Hay(HnomKs)
 
 
