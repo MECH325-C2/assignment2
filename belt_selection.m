0e58ec5f83231d0434e94f3ea9a8708efd84c178
Morten Kals <m.kals@me.com>
2016-10-15 23:38:45 -0700

    added belt drive that is close to working, but high cost

--- belt_selection.m
+++ belt_selection.m
@@ -1,10 +1,12 @@
 function [  ] = belt_selection( )
 %%
 % Values from example 17-1 in book to confirm correct operation
-length = belt_test([6, 18, 96, 1750, 6, 0.13, 15, 1.25, 1.1, 0.70, 1, 100, 0.042, 0.8]);
+% length = belt_test([6, 18, 96, 1750, 6, 0.13, 15, 1.25, 2.5, 0.70, 1, 100, 0.042, 0.8]);
 
-%length = belt_test([2, 20, 30, 1200, 3, 0.062, 1, 1.25, 1.05, 0.95, 1, 5.2, 0.038, 0.7]);
+%%
+% Possible settup: 6:60 pulleys, 
+length = belt_test([6, 60, 30, 1200, 6, 0.266, 1, 1.25, 1.05, 0.6, 1, 41, 0.045, 0.4]);
 
-price = 340+71+4.58*length;
-
-disp(['total price: ', num2str(price)]);
+price = 188/8*60+96+57.88*length;
+% part 5706K34 pulleys, 6080K22 2ply leather belt
+disp(['total price: ', num2str(price)]); % Price very unreasonable!
