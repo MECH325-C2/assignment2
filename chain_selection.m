c43c05d7e16fe126e0aaed2815379e81361947a9
Morten Kals <m.kals@me.com>
2016-10-17 19:39:14 -0700

    minor changes and readability updates

--- chain_selection.m
+++ chain_selection.m
@@ -9,23 +9,23 @@ h = (2+2)*4*250*4;          % hours of operation
 disp(['Hours of operation: ', num2str(h)]);
 
 %% First stage drive, ~2:1 ratio
-chain_length = chain_test([35, 21, 40, 1200, 8, h, 1, 1]);
+chain_length = chain_test([35, 21, 40, 1200, 8, h*2.5, 1, 1]);
 cost_1 = 14.65;             % driving: 6793K129
 cost_1 = cost_1 + 28.13;    % driven: 6793K137
 cost_1 = cost_1 + 70.64;    % chain tensioner: 5896K1
 
 cost_per_foot = 3.90;       % chain: 6261K172
-cost_1 = cost_1 + ceil(chain_length/12)*cost_per_foot + 0.82; % total cost of chain + link
+cost_1 = cost_1 + ceil(chain_length/12)*cost_per_foot + 0.82 % total cost of chain + link
 
 
 %% Second stage drive, ~5:1 ratio
-chain_length = chain_test([40, 17, 89, 1200/40*21, 22, h, 1, 1]);
+chain_length = chain_test([40, 17, 89, 1200/40*21, 22, h*2.5, 1, 1]);
 cost_2 = 16.08;                 % driving: 6793K148
 cost_2 = cost_2 + 57.09*89/60;  % driven: 6793K163, linear scaling from 60 to 89 teeth 
 cost_2 = cost_2 + 68.62;        % chain tensioner: 5896K2
 
 cost_per_foot = 4.54;           % chain: 6261K173
-cost_2 = cost_2 + ceil(chain_length/12)*cost_per_foot + 0.87; % total cost of chain + link
+cost_2 = cost_2 + ceil(chain_length/12)*cost_per_foot + 0.87 % total cost of chain + link
 
 
 %% Extra Costs
